c104407a2e800b7cde1f6678278c89225267093c
grobian <gordon-fischer@gmx.de>
2013-06-06 17:51:03 +0300

    Update FilterPlusTable.js

--- FilterPlusTable.js
+++ FilterPlusTable.js
@@ -75,10 +75,10 @@ function filterAll()
     console.log("Typ:("+Typ+") & Material("+Material+"): "+filterTypMaterial.length);
     console.log("Material("+Material+") & Datum("+Datum+"): "+filterDatumMaterial.length);
     console.log("Datum("+Datum+") & Typ("+Typ+"): "+filterDatumTyp.length);
-    var filterdata = '{'
+    var filterdata = '[{'
        +'"ID" : data[0].ID,'
        +'"dating"  : data[0].dating,'
-       +'}';
+       +'}]';
     
     //	filterdata.push({"ID":data[0].ID, "dating":data[0].dating});
     	SundialsListCtrl(filterdata);
